a2dd0710778cdbeb2c7c48805f07adbda4417551
William Bowling <william.bowling@equella.com>
2012-08-20 16:25:03 +1000

    Option to play beeps for all messages in groupchats

---
+++ js/groupchat-beep.js
@@ -0,0 +1,40 @@
+$(function() {
+
+
+$("body").delegate("#page-engine", "messageShown", function(data){
+
+    if(getDB('options', 'groupchat-beep') == '0')
+		return true;
+		
+	if (data.chatType == 'groupchat')
+	{
+	    soundPlay(1);
+	}
+});
+
+
+$(document).delegate("body", "optionsPopup", function(){
+        $('<fieldset><legend>Groupchat beeps</legend><label for="groupchat-beeps">Play a sound when there is a new message in groupchats</label><input id="groupchat-beeps" type="checkbox" /></fieldset>').appendTo("#conf1");
+});
+
+$(document).delegate("body", "loadOptions", function(){
+	if(getDB('options', 'groupchat-beep') == '0')
+		$('#groupchat-beep').attr('checked', false);
+	else
+		$('#groupchat-beep').attr('checked', true);
+});
+
+$(document).delegate("body", "saveOptions", function(){
+    var groupchat-beep = '0';
+    if($('#groupchat-beep').filter(':checked').size())
+		groupchat-beep = '1';
+	setDB('options', 'groupchat-beep', groupchat-beep);
+    
+});
+
+$(document).delegate("body", "storeOptions", function(event){
+	event.oType.push("groupchat-beep");
+    event.oContent.push(getDB('options', 'groupchat-beep'));
+});
+
+});
